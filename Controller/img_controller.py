f03b2ae6fc8695c3d393abb913bc71da3aa6c6d7
MiHyeon <kkmmhh7478@gmail.com>
2023-11-09 01:07:08 +0900

    controller 구분

--- Controller/img_controller.py
+++ Controller/img_controller.py
@@ -1,26 +1,9 @@
 import os
-import class_detect as det
-
 import crop
 import cv2
+import opencv_ration_resize_class as resize
 
-import resize
-
-class Controller:
-
-    # 객체 탐지 컨트롤러
-    def detect_con(img_path):
-        detect = det.detect_class(img_path)
-        detect.detect_run()
-        print(detect.get_object_all_locatin)
-        print(detect.get_vulnerability)
-
-        for key,value in detect.object_location_dict.items():
-            print("key : {} , value : {}".format(key,value))
-
-        detect.get_bound_box()
-        detect.draw_run()
-        detect.get_label_location()
+class img_controller:
 
     # 크롭 컨트롤러
     def crop_con(img_path, label_txt):
