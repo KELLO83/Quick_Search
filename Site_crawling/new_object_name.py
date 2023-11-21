55adca7c6a6c7f776b8d4361fdfd51ac1c2bd82b
skyblizzard64@gmail.com <skyblizzard64@gmail.com>
2023-11-21 23:18:07 +0900

    다시수정 (확인중)

--- Site_crawling/new_object_name.py
+++ Site_crawling/new_object_name.py
@@ -29,33 +29,47 @@ def upload_image_and_extract_keyword(image_path) -> str:
     # 이미지 파일의 로컬 경로 지정
     file_input.send_keys(image_path)
     time.sleep(2)
-    
+    try:
+        button_is_existed = driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[2]/c-wiz/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div[2]/div/div/a')
+        direct_name = button_is_existed.accessible_name
+        direct_name = direct_name.replace("검색","")
+        return direct_name
+    except:
+        print("바로검색 불가능")
+
     
     # 키워드 추출
-    keyword_list = []
-    result = driver.find_elements(By.CSS_SELECTOR,"div.UAiK1e")
-    for i in result:
-        keyword_list.append(i.text)
-        
-    keyword_list = [re.sub('[^0-9ㄱ-힗]', '', _) for _ in keyword_list]
-    nltk.download('punkt')
-    words = []
-    for text in keyword_list:
-        words.extend(word_tokenize(text))
-    words_counts = Counter(words)
+
+    test = driver.find_elements(By.CSS_SELECTOR,'div.Vd9M6')
+    filter_list = []
     
-    most_word = words_counts.most_common(10)
-    result = []
-    for i in range(most_word.__len__()):
-        result.append(most_word[i][0])
-
-    result = [re.sub(r"\d{7}", "", _) for _ in result]
-    result = [item for item in result if not item.isdigit()]
-               
-    print(result)
+    for i in test:
+        try:
+            span = i.find_element(By.CSS_SELECTOR,'span.DdKZJb')
+            filter_list.append(i)
+        except:
+            pass
+        
+    proudct_name = []
+    for i in filter_list:
+        try:
+            name = i.find_element(By.CSS_SELECTOR,'div.UAiK1e')
+            proudct_name.append(name)
+        except:
+            pass
+    
+    real_name = []
+    for i in proudct_name:
+        try:
+            real_name.append(i.text)
+        except:
+            pass
+        
+    print(real_name)
+    
+    return real_name
+                
     driver.quit()  # WebDriver 종료
-    return result
-
 if __name__ == "__main__":
     print("TEST CODE 입니다 ....")
     img_path = "C:/Users/user/Quick_Search/Site_crawling/cap.jpg"  # 이미지경로 TEST PATH
@@ -65,4 +79,4 @@ if __name__ == "__main__":
         print("File Not Exist")
         raise  FileNotFoundError
     keyword = upload_image_and_extract_keyword(img_path) # Site_crawling/target.jpg
-    # print(keyword)
+    print("keword : ",keyword)
